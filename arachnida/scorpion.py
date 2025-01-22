158bc2bac645fe822c17c449a325a15abecb26be
John Decorte <john.decorte01@gmail.com>
2025-01-22 12:04:52 +0100

    scorpion in the pocket

--- arachnida/scorpion.py
+++ arachnida/scorpion.py
@@ -1,18 +1,40 @@
-import argparse
-import PIL.Image
+import sys
+from PIL import Image
+from PIL.ExifTags import TAGS
+import time
+import os
 
 def readExif(filename):
-    with PIL.Image.open(filename) as img:
-        exif_data = img.getexif()
-        metadata = {}
-        for tag, value in exif_data.items():
+    try:
+        with Image.open(filename) as img:
+            exif_data = img.getexif()
+            for tag, value in exif_data.items():
+                if tag in TAGS:
+                    print(f' * {TAGS[tag]}: {value}')
+                else:
+                    print(f' * {tag}: {value}')
+    except Exception as e:
+        print("Error reading exif data: ", filename, e)
 
 
+def scorpion():
+    args = sys.argv
+    if(len(args) <= 1):
+        return print("Error: scorpion need at least 1 file")
+    
+    supported_format = ('.jpg', '.jpeg', '.png', '.gif', '.bmp')
+
+    for i in range(1, len(args)):
+        if args[i].endswith(supported_format):
+            print(f"\nMetaData for {args[i]}")
+            print(f"Creation date: {time.ctime(os.path.getctime(args[i]))}")
+            print(f"Modification date: {time.ctime(os.path.getmtime(args[i]))}")
+            print(f"Size file (bytes): {os.path.getsize(args[i])}")
+            readExif(args[i])
+        else:
+            print("\nError: Unsupported file format", args[i])
 
 if __name__ == "__main__":
-    argparser = argparse.ArgumentParser()
-    argparser.add_argument("FILE1")
-    args = argparser.parse_args()
-    readExif(args.FILE1)
+    scorpion()
 
     
